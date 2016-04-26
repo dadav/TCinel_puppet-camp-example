aee4811053ed594b9bc8f94cde6d22148a3cd847
Tim Cinel <tcinel@atlassian.com>
2016-04-27 01:35:32 +1000

    Put nexus_client and extdirect into a Nexus3 module

--- lib/puppet_x/nexus3/nexus_client.rb
+++ lib/puppet_x/nexus3/nexus_client.rb
@@ -1,43 +1,38 @@
 require 'faraday'
 require 'uri'
-require 'json'
 require 'base64'
 
-require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'nexus3', 'extdirect.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'puppet_x', 'nexus3', 'extdirect.rb'))
 
-class NexusClient
-  def initialize(base_url, admin_user, admin_password)
-    @base_url = ::URI.parse(base_url).to_s
-    @admin_user = admin_user
-    @admin_password = admin_password
-  end
-
-  def session
-    @session || begin
-      conn = Faraday.new(:url => "#{@base_url}") do |faraday|
-        faraday.request  :url_encoded
-        faraday.adapter  Faraday.default_adapter
-      end
-
-      response = conn.post '/service/rapture/session', {
-        :username => Base64.urlsafe_encode64(@admin_user),
-        :password => Base64.urlsafe_encode64(@admin_password),
-        :rememberMe => 'on',
-      }
-
-      raise "Unable to Authenticate" unless /NXSESSIONID/ =~ response.headers['set-cookie']
-
-      @session = /NXSESSIONID=([^;]+);/.match(response.headers['set-cookie'])[1]
+module Nexus3
+  class NexusClient
+    def initialize(base_url, admin_user, admin_password)
+      @base_url = ::URI.parse(base_url).to_s
+      @admin_user = admin_user
+      @admin_password = admin_password
     end
-  end
 
-  def remote(*args, &block)
-    self.extdirect_client.remote(*args, &block)
-  end
+    def session
+      @session ||= begin
+        conn = Faraday.new(:url => "#{@base_url}") do |faraday|
+          faraday.request  :url_encoded
+          faraday.adapter  Faraday.default_adapter
+        end
 
-  def extdirect_client
-    @extdirect_client || begin
-      @extdirect_client = ::ExtDirect.new(
+        response = conn.post '/service/rapture/session', {
+          :username => Base64.urlsafe_encode64(@admin_user),
+          :password => Base64.urlsafe_encode64(@admin_password),
+          :rememberMe => 'on',
+        }
+
+        raise "Unable to Authenticate" unless /NXSESSIONID/ =~ response.headers['set-cookie']
+
+        /NXSESSIONID=([^;]+);/.match(response.headers['set-cookie'])[1]
+      end
+    end
+
+    def extdirect_client
+      @extdirect_client ||= Nexus3::ExtDirect.new(
         "#{@base_url}/service/extdirect",
         {:cookie => "NXSESSIONID=#{self.session}"},
       )
