a993911029ecce7ca4fa76f225c1bbc70fc9188f
Tim Cinel <tcinel@atlassian.com>
2016-04-27 04:03:45 +1000

    Added a new 'online' property to the nexus3_repository type. Had to introduce flushing and munging for this to work properly.

--- lib/puppet/provider/nexus3_repository/rpc.rb
+++ lib/puppet/provider/nexus3_repository/rpc.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'nexus3', 'client.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppet_x', 'nexus3', 'util.rb'))
 
 Puppet::Type.type(:nexus3_repository).provide(:rpc) do
   MAVEN_REPO_SKELETON = {
@@ -20,6 +21,11 @@ Puppet::Type.type(:nexus3_repository).provide(:rpc) do
     :recipe => 'maven2-hosted',
   }
 
+  def initialize(value={})
+    super(value)
+    @flush_required = false
+  end
+
   def self.instances
     repositories = Nexus3::Client.remote('coreui_Repository', 'read', nil)['result']['data']
 
@@ -28,7 +34,8 @@ Puppet::Type.type(:nexus3_repository).provide(:rpc) do
     }.collect { |repository|
       new(
         :ensure => :present,
-        :name   => repository['name']
+        :name   => repository['name'],
+        :online => Nexus3::Util.munge_boolean(repository['online']),
       )
     }
   end
@@ -47,13 +54,43 @@ Puppet::Type.type(:nexus3_repository).provide(:rpc) do
   end
 
   def create
-    result = Nexus3::Client.remote('coreui_Repository', 'create',
-      [MAVEN_REPO_SKELETON.merge({:name => resource[:name]})]
-    )
+    result = Nexus3::Client.remote('coreui_Repository', 'create', map_resource_to_data)
+    Puppet::debug("nexus3_repository create result: #{result}")
   end
 
   def destroy
     result = Nexus3::Client.remote('coreui_Repository', 'remove', [resource[:name]])
+    Puppet::debug("nexus3_repository destroy result: #{result}")
+  end
+
+  def flush
+    if @flush_required
+      result = Nexus3::Client.remote('coreui_Repository', 'update', map_resource_to_data)
+
+      Puppet::debug("nexus3_repository update result: #{result}")
+      fail() unless result['result']['success']
+
+      @property_hash = resource.to_hash
+    end
+  end
+
+  def map_resource_to_data
+    [
+      MAVEN_REPO_SKELETON.merge(
+        {
+          :name   => resource[:name],
+          :online => Nexus3::Util.sym_to_bool(resource[:online]),
+        }
+      )
+    ]
+  end
+
+  def online
+    @property_hash[:online]
+  end
+
+  def online=(value)
+    @flush_required = true
   end
 
 end
--- lib/puppet/type/nexus3_repository.rb
+++ lib/puppet/type/nexus3_repository.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'puppet_x', 'nexus3', 'client.rb'))
+
 Puppet::Type.newtype(:nexus3_repository) do
   desc 'Manage hosted repositories in Nexus 3'
 
@@ -5,6 +7,13 @@ Puppet::Type.newtype(:nexus3_repository) do
 
   newparam(:name, :namevar => true) do
     desc 'The name of the hosted repository'
+  end
+
+  newproperty(:online) do
+    desc 'Determines repository availability'
+    newvalues(:true, :false)
+    defaultto :true
+    munge { |value| Nexus3::Util.munge_boolean(value) }
   end
 
 end
