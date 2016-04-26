4c6027a8b9619c507d02ee9ae49eb0c6645af481
Tim Cinel <tcinel@atlassian.com>
2016-04-27 00:38:26 +1000

    Added inline documentation

--- lib/puppet/type/nexus3_repository.rb
+++ lib/puppet/type/nexus3_repository.rb
@@ -1,8 +1,10 @@
 Puppet::Type.newtype(:nexus3_repository) do
+  desc 'Manage hosted repositories in Nexus 3'
 
   ensurable
 
   newparam(:name, :namevar => true) do
+    desc 'The name of the hosted repository'
   end
 
 end
