6769e139323840d1d1d1ba18117b735c264967a2
Tim Cinel <tcinel@atlassian.com>
2016-04-26 20:44:31 +1000

    Initial commit. Basic extdirect client and Nexus Client with auth ability

---
+++ extdirect.rb
@@ -0,0 +1,37 @@
+require 'faraday'
+require 'uri'
+require 'json'
+
+class ExtDirect
+  def initialize(url, headers = {})
+    @url = ::URI.parse(url).to_s
+    @headers = headers
+    @tid = 0
+
+    @headers[:content_type] = 'application/json'
+  end
+
+  def connection
+    @connection || ::Faraday.new()
+  end
+
+  def remote(action, method, data)
+    @tid += 1
+
+    body = {
+      'type'   => 'rpc',
+      'tid'    => @tid,
+      'action' => action,
+      'method' => method,
+      'data'   => data,
+    }.to_json
+
+    response = self.connection.post(@url, body, @headers)
+
+    if response.nil? || response.body.nil? || response.body.empty?
+      raise "Error Making Request"
+    end
+
+  end
+
+end
---
+++ nexus_client.rb
@@ -0,0 +1,42 @@
+require 'faraday'
+require 'uri'
+require 'json'
+require 'base64'
+
+require File.expand_path('extdirect.rb')
+
+class NexusClient
+  def initialize(base_url, admin_user, admin_password)
+    @base_url = ::URI.parse(base_url).to_s
+    @admin_user = admin_user
+    @admin_password = admin_password
+  end
+
+  def session
+    @session || begin
+      conn = Faraday.new(:url => "#{@base_url}") do |faraday|
+        faraday.request  :url_encoded
+        faraday.adapter  Faraday.default_adapter
+      end
+
+      response = conn.post '/service/rapture/session', {
+        :username => Base64.urlsafe_encode64(@admin_user),
+        :password => Base64.urlsafe_encode64(@admin_password),
+        :rememberMe => 'on',
+      }
+
+      raise "Unable to Authenticate" unless /NXSESSIONID/ =~ response.headers['set-cookie']
+
+      @session = /NXSESSIONID=([^;]+);/.match(response.headers['set-cookie'])[1]
+    end
+  end
+
+  def extdirect_client
+    @extdirect_client || begin
+      @extdirect_client = ::ExtDirect.new(
+        "#{@base_url}/service/extdirect",
+        {'NXSESSIONID' => self.session},
+      )
+  end
+
+end
